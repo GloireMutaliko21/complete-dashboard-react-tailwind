085b737b80ab63a006cdb0b7c5ad282bf768376a
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-07-28 14:19:28 +0200

    feat: initial contextAPI implementation

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import ReactDOM from "react-dom";
 
 import './index.css'
 import App from './App';
+import { ContextProvider } from "./contexts/ContextProvider";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render
+    (
+        <ContextProvider>
+            <App />
+        </ContextProvider>,
+        document.getElementById('root')
+    );
