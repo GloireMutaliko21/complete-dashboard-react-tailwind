371341afb31653af0214f168c2c7dded07163866
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-07-28 14:19:28 +0200

    feat: initial contextAPI implementation

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import { Navbar, Footer, Sidebar, ThemeSettings, } from "./components";
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Pie, Line, Bar, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 
+import { useStateContext } from "./contexts/ContextProvider";
+
 import './App.css'
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
 
   return (
     <div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,10 +5,10 @@ import { MdOutlineCancel } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { links } from '../data/dummy';
-import { itemsToOrder } from '@syncfusion/ej2/treemap';
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const activeMenu = true;
+  const { activeMenu, setActiveMenu } = useStateContext();
 
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
@@ -20,7 +20,7 @@ const Sidebar = () => {
           <div className='flex justify-between items-center'>
             <Link
               to='/'
-              onClick={() => { }}
+              onClick={() => setActiveMenu(false)}
               className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white to-slate-900 '
             >
               <SiShopware /> <span>Shoppy</span>
@@ -31,7 +31,7 @@ const Sidebar = () => {
             >
               <button
                 type='button'
-                onClick={() => { }}
+                onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
                 className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'
               >
                 <MdOutlineCancel />
