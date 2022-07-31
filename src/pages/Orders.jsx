d6a5ae6cb2ad23a390ecec0ee8e9cba64f9762fe
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-07-31 18:04:05 +0200

    allow data sorting and pagination

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from "@syncfusion/ej2-react-grids";
 import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
 import { Header } from "../components";
+import { pdfexport } from '@syncfusion/ej2';
 
 const Orders = () => {
   return (
@@ -11,11 +12,15 @@ const Orders = () => {
       <GridComponent
         id='gridcomp'
         dataSource={ordersData}
+        allowPaging
+        allowSorting
       >
         <ColumnsDirective>
-          {ordersGrid.map((item, index) =>
-            <ColumnDirective key={index} {...item} />)}
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
         </ColumnsDirective>
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit]} />
       </GridComponent>
     </div>
   )
