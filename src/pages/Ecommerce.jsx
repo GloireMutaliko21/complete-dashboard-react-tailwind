5af11b7d0db8b9fecaa8d228c6696deb0f7f5ed6
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-07-31 09:41:30 +0200

    style: adding earningData cart

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -40,9 +40,21 @@ const Ecommerce = () => {
               <button
                 type='button'
                 style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+                className='text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl'
               >
                 {item.icon}
               </button>
+              <p className='mt-3'>
+                <span className='text-lg font-semibold'>
+                  {item.amount}
+                </span>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  {item.percentage}
+                </span>
+              </p>
+              <p className='text-sm text-gray-400 mt-1'>
+                {item.title}
+              </p>
             </div>
           ))}
         </div>
