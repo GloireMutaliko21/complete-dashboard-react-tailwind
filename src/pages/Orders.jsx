102f5e5d8ffac7b26d0230f2ccde44ddfe0813c9
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-07-31 17:29:07 +0200

    style: design Header component

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from "@syncfusion/ej2-react-grids";
+import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Orders = () => {
   return (
-    <div>Orders</div>
+    <div>
+      <Header category="Page" title="Orders" />
+    </div>
   )
 }
 
