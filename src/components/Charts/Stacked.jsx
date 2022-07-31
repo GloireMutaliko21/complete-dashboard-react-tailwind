3102e35c9bf1a5a99d27313cc607b5d53025f096
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-07-31 17:07:29 +0200

    style: put the stacked chart

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -3,10 +3,23 @@ import React from 'react';
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from "@syncfusion/ej2-react-charts";
 import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from "../../data/dummy";
 
-const Stacked = () => {
+const Stacked = ({ width, height }) => {
   return (
-    <ChartComponent>
+    <ChartComponent
+      width={width}
+      height={height}
+      id='charts'
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      legendSettings={{ background: 'white' }}
+    >
       <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) =>
+          <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
     </ChartComponent>
   )
 }
