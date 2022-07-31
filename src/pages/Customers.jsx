a6aa5df61351f86aaaad1a95fe213d9cc39ec69a
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-07-31 18:40:14 +0200

    style adding customers page

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from "@syncfusion/ej2-react-grids";
+import { customersData, customersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Customers = () => {
   return (
-    <div>Customers</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title="Customers" />
+      <GridComponent
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        toolbar={['Search', 'Delete']}
+        editSettings={{ allowDeleting: true, allowEditing: true }}
+        width='auto'
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Sort, Page, Selection, Edit, Filter, Toolbar]} />
+      </GridComponent>
+    </div>
   )
 }
 
