7d2e0877bd47bbbf7e97e3bf86cf5f505a72829c
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-07-31 18:20:10 +0200

    design employees page

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Sort, Page, Search, Toolbar, Inject } from "@syncfusion/ej2-react-grids";
+import { employeesData, contextMenuItems, employeesGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Employees = () => {
   return (
-    <div>Employees</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={['Search']}
+        width='auto'
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Sort, Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
   )
 }
 
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from "@syncfusion/ej2-react-grids";
 import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
 import { Header } from "../components";
-import { pdfexport } from '@syncfusion/ej2';
 
 const Orders = () => {
   return (
