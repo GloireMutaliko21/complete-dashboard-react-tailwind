3b77c063ae5c0db534942a8ea77f211b2b48d555
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-08-01 20:04:29 +0200

    fix: sparkline component

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,33 +1,37 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
+import React from 'react';
 import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
-  return (
-    <SparklineComponent
-      id={id}
-      height={height}
-      width={width}
-      lineWidth={1}
-      valueType="Numeric"
-      fill={color}
-      border={{ color: currentColor, width: 2 }}
-      dataSource={data}
-      xName="x"
-      yName="y"
-      type={type}
-      tooltipSettings={{
-        visible: true,
-        // eslint-disable-next-line no-template-curly-in-string
-        format: '${x} : data ${y}',
-        trackLineSettings: {
-          visible:true
-        }
-      }}
-    >
-      <Inject services={[SparklineTooltip]} />
-    </SparklineComponent>
-  )
+class SparkLine extends React.PureComponent {
+  render() {
+    const { id, height, width, color, data, type, currentColor } = this.props;
+
+    return (
+      <SparklineComponent
+        id={id}
+        height={height}
+        width={width}
+        lineWidth={1}
+        valueType="Numeric"
+        fill={color}
+        border={{ color: currentColor, width: 2 }}
+        tooltipSettings={{
+          visible: true,
+          // eslint-disable-next-line no-template-curly-in-string
+          format: '${x} : data ${yval}',
+          trackLineSettings: {
+            visible: true,
+          },
+        }}
+        markerSettings={{ visible: ['All'], size: 2.5, fill: currentColor }}
+        dataSource={data}
+        xName="x"
+        yName="yval"
+        type={type}
+      >
+        <Inject services={[SparklineTooltip]} />
+      </SparklineComponent>
+    );
+  }
 }
 
-export default SparkLine
+export default SparkLine;
