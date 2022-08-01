e3311b44166d9a4257224a4488e2e102ceb5d26e
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-08-01 19:32:45 +0200

    adding LineChart page

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, Legend, Tooltip, LineSeries } from "@syncfusion/ej2-react-charts";
+import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const LineChart = () => {
+  const { currentMode } = useStateContext();
+
   return (
-    <div>LineChart</div>
+    <ChartComponent
+      id='line-chart'
+      height='420px'
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? "#33373E" : "#fff"}
+    >
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) =>
+          <SeriesDirective key={index} {...item} />
+        )}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   )
 }
 
--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Header, LineChart } from "../../components";
 
 const Line = () => {
   return (
-    <div>Line</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header category='Chart' title='Inflation Rate' />
+      <div className='w-full'>
+        <LineChart />
+      </div>
+    </div>
   )
 }
 
