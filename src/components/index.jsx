ca4e45980b4de9066030783a5a94279a451dd407
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-08-02 15:48:26 +0200

    adding chat component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -12,5 +12,4 @@ export { default as SparkLine } from './Charts/SparkLine';
 export { default as LineChart } from './Charts/LineChart';
 export { default as Stacked } from './Charts/Stacked';
 export { default as PieChart } from './Charts/PieChart';
-export { default as ChartsHeader } from './ChartHeader';
 export { default as Header } from './Header';
