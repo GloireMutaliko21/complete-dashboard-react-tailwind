6b7959dceb8f73a3d5fd70428b3fab805bf7d723
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-08-02 09:52:41 +0200

    adding barChart page

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, Tooltip, ColumnSeries, DataLabel } from '@syncfusion/ej2-react-charts';
+import { barCustomSeries, barPrimaryXAxis, barPrimaryYAxis } from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
+import { Header } from "../../components";
 
 const Bar = () => {
+  const { currentMode } = useStateContext();
+
   return (
-    <div>Bar</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header category="Chart" title="Bar" />
+      <ChartComponent
+        id='bar-chart'
+        height='420px'
+        primaryXAxis={barPrimaryXAxis}
+        primaryYAxis={barPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        background={currentMode === 'Dark' ? "#33373E" : "#fff"}
+        // legendSettings={{ background: 'white' }}
+      >
+        <Inject services={[ColumnSeries, Legend, Tooltip, Category, DataLabel]} />
+        <SeriesCollectionDirective>
+          {barCustomSeries.map((item, index) =>
+            <SeriesDirective key={index} {...item} />
+          )}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+
   )
 }
 
